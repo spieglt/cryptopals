6f856e6b34b41f06f65ff0c5d90a0413281ad004
Theron <tspiegl@gmail.com>
2018-12-05 23:53:51 -0600

    ex23

--- src/set3/ex23.rs
+++ src/set3/ex23.rs
@@ -20,66 +20,6 @@ How would you modify MT19937 to make this attack hard? What would happen if you 
 
 */
 
-// given = x ^ (x >> 18)
-// how to get x? we're looking for a number that, when xor'd with itself bitshifted, gives us known number
-// if i have 				10100101011010101010010101101101, and right-shift by 18,
-// then i have				00000000000000000010100101011010
-
-// xor of these = given		10100101011010101000110000110111
-
-// so from given, i know that top 18 bits are from the original,
-// and the bottom 14 bits should be the xor of bottom 14 bits of given and top 14 bits of given
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-// what about 11?		
-// if i have 				10100101011010101010010101101101, and right-shift by 11,
-// then i have				00000000000101001010110101010100
-
-//							gggggggggggbbbbbbbbbbbbbbbbbbbbb
-// xor of these = given		10100101011111100000100000111001
-
-// so i know top 11 are good, next 11 need to be xor'd with first 11, then i have 22
-//							10100101011010101010010000111001
-
-// then i can start over, knowing that the bottom 10 will be (bottom 10 of given ^ bottom 10 of top 32 - 11)
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-use set3::ex21;
-
-use std::cmp::min;
-
-fn bit_length(inp: &u32) -> u32 {
-	match inp {
-		0 => f32::from_bits(*inp).log2() as u32,
-		_ => f32::from_bits(*inp).log2() as u32 + 1,
-	}
-}
-
-fn undo_xor_with_right_shift(given: u32, rsv: usize) -> u32 {
-	assert!(rsv <= 32);
-
-	let mut known_bits = given >> (32 - rsv);	// must be left-shifted when appended to
-	
-	while bit_length(&known_bits) < 32 {
-		// to mask off high bits, get known bits on the left, then xor it
-		let remainder = given ^ (known_bits << (32 - bit_length(&known_bits)));	// remainder == given without known bits
-		// want to grab up to bit_length(known_bits) top bits of remainder (get max)
-		let num_working_bits = min(bit_length(&known_bits), bit_length(&remainder));
-		let working_known_bits = known_bits >> (bit_length(&known_bits) - num_working_bits);
-		let chopped_remainder = remainder >> (bit_length(&remainder) - num_working_bits);
-		// then xor them with max result number of known bits, then left shift known_bits by bit_length of extension, then add extension
-		let extension = working_known_bits ^ chopped_remainder;
-		known_bits = known_bits << bit_length(&extension) + extension;
-	}
-	known_bits
-}
-
-fn untemper(y: u32) -> u32 {
-	0
-}
-
 // pub fn extract_number(&mut self) -> Result<u32, String> {
 // 	if self.index >= self.n {
 // 		if self.index > self.n {
@@ -97,23 +37,73 @@ fn untemper(y: u32) -> u32 {
 // 	Ok(y)
 // }
 
+use set3::ex21;
+
+use std::cmp::min;
+
+
+fn low_n_bits(inp: u32, n: u32) -> u32 {
+	inp ^ ((inp >> n) << n)
+}
+
+fn undo_xor_with_right_shift(given: u32, rsv: u32) -> u32 {
+	assert!(rsv <= 32);
+
+	let mut known_bits = given >> (32 - rsv);	// must be left-shifted when appended to
+	let mut num_known_bits = rsv;
+	let mut i = 0;
+	while num_known_bits < 32 {
+		let num_remainder_bits = 32 - num_known_bits;
+		let remainder = low_n_bits(given, num_remainder_bits);
+		// grab up to rsv bits to work on
+		let num_working_bits = min(rsv, num_remainder_bits);
+		let next_chunk = remainder >> (32 - num_known_bits - num_working_bits);
+		let relevant_bits = low_n_bits(known_bits, num_known_bits - (rsv * i)) >> (rsv - num_working_bits);
+		let extension = relevant_bits ^ next_chunk;
+		known_bits = (known_bits << num_working_bits) + extension;
+		num_known_bits += num_working_bits;
+		i += 1;
+	}
+	known_bits
+}
+
+fn untemper(y: u32) -> u32 {
+	0
+}
+
+
 pub fn clone_mt19947_prng() {
 	let mut target = ex21::MtPrng::new();
 	target.seed_mt(5);
-
 
 }
 
 
 #[cfg(test)]
 mod tests {
+	use set3::ex21;
+
 	#[test]
 	fn test_undo_xor_with_right_shift() {
-		for i in 0..1000 {
+		let mut mt = ex21::MtPrng::new();
+		mt.seed_mt(3210952839);
+		let mut test_vec = vec![];
+		for _ in 0..1000 {
+			let mut temp_val = mt.extract_number().unwrap();
+			test_vec.push(temp_val);
+		}
+		println!("{:?}", test_vec);
+		for val in test_vec {
+			println!("on {}", val);
 			for rsv in 1..32 {
-				let modified = i ^ (i >> rsv);
-				assert_eq!(i, super::undo_xor_with_right_shift(modified, rsv));
+				println!("rsv {}", rsv);
+				let modified = val ^ (val >> rsv);
+				assert_eq!(val, super::undo_xor_with_right_shift(modified, rsv));
 			}
 		}
 	}
 }
+
+// fn bit_length(inp: &u32) -> u32 {
+// 	(*inp as f32).log2() as u32 + 1
+// }
