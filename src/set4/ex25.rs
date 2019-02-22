5aee3e7e8a36b39dde192cfeaa61d7f0ea3ddcef
Theron <tspiegl@gmail.com>
2019-02-22 17:14:35 -0600

    made ex25 faster, though still slow

--- src/set4/ex25.rs
+++ src/set4/ex25.rs
@@ -15,11 +15,12 @@ A folkloric supposed benefit of CTR mode is the ability to easily "seek forward"
 
 */
 
-use crate::{ex7, ex11, ex18};
+use crate::{ex7, ex10, ex11, ex18};
 use crate::utils;
 use rand::{thread_rng, Rng};
 use std::io;
 use std::io::Write;
+use std::mem::transmute;
 
 // attacker controls offset and new text. ciphertext is xor of plaintext and keystream. keystream is aes of key and nonce + counter. we know counter.
 // we don't know nonce but we do because we who are writing the edit function have the key, we're just exposing the function. so we have key.
@@ -37,30 +38,56 @@ impl CtrEncrypter {
 	fn new(key: &Vec<u8>, nonce: &Vec<u8>) -> CtrEncrypter {
 		CtrEncrypter{key: key.clone(), nonce: nonce.clone()}
 	}
-	fn edit(&self, ciphertext: &Vec<u8>, offset: &u64, newtext: &Vec<u8>) -> Vec<u8> {
-		// TODO: calculate block # of offset and # of blocks covered?
-		// let low_block = offset / 16;
-		// let high_block = (offset + newtext.len()) / 16;
+	fn edit(&self, ciphertext: &Vec<u8>, offset: usize, newtext: &Vec<u8>) -> Vec<u8> {
+		assert!(offset + newtext.len() < ciphertext.len());
+		let block_size = 16;
+		let low_block = offset / block_size;
+		let high_block = (offset + newtext.len()) / block_size;
+		let num_blocks = 1 + high_block - low_block;
 		// need keystream for those blocks, so need nonce and to transmute block nums, then aes with key
-		// then grab those blocks from ciphertext, xor, edit, re-xor, put back into ciphertext
-
-		// for now, going to be lazy and just unencrypt/reencrypt the whole thing.
-		let mut plaintext = ex18::encrypt_ctr(&ciphertext, &self.key, &self.nonce);
-		for (i, b) in newtext.iter().enumerate() {
-			plaintext[*offset as usize + i] = *b;
+		let mut keystream: Vec<u8> = vec![];
+		for block_num in low_block..=high_block {
+			let bytes: [u8; 8] = unsafe { transmute(block_num.to_le()) };
+			let mut bv = bytes.to_vec();
+			let mut material = &mut self.nonce.clone();
+			material.append(&mut bv);
+			ex10::encrypt_aes128ecb(&mut material, &self.key).expect("couldn't encrypt with ecb");
+			keystream.append(material);
 		}
-		ex18::encrypt_ctr(&plaintext, &self.key, &self.nonce)
+		// then grab those blocks from ciphertext, xor, edit, re-xor, put back into ciphertext
+		let mut text_to_edit = ciphertext[low_block*block_size..
+				utils::min(ciphertext.len(), (low_block+num_blocks)*block_size) // don't reach past end of ciphertext
+			].to_vec();
+		for i in 0..text_to_edit.len() {
+			text_to_edit[i] = text_to_edit[i] ^ keystream[i];
+		}
+		// text_to_edit is now in plaintext form, time to edit
+		let local_offset = offset % block_size;
+		for (i, b) in newtext.iter().enumerate() {
+			text_to_edit[local_offset + i] = *b;
+		}
+		// re-xor with keystream
+		for i in 0..text_to_edit.len() {
+			text_to_edit[i] = text_to_edit[i] ^ keystream[i];
+		}
+		// reconstruct ciphertext
+		let mut new_ciphertext = ciphertext[..block_size*low_block].to_vec();
+		new_ciphertext.append(&mut text_to_edit);
+		let mut original_end = ciphertext[utils::min(ciphertext.len(), block_size*(high_block+1))..].to_vec();
+		new_ciphertext.append(&mut original_end);
+		new_ciphertext
 	}
 }
 
 pub fn break_random_access_read_write() {
+	let block_size = 16;
+
 	let plaintext_b64 = String::from_utf8(utils::read_file("./src/resources/25.txt")).expect("could not convert to string");
 	let mut plaintext = utils::base64_to_bytes(&plaintext_b64);
 
 	// misread the prompt. thought "the recovered plaintext from this file (the ECB exercise)" meant recovered by base64-decoding,
 	// but in ex7, the ecb exercise, this was still encrypted after decoding. so we need to ecb decrypt it.
 	ex7::decrypt_aes128ecb(&mut plaintext, "YELLOW SUBMARINE".as_bytes()).expect("could not decrypt");
-
 	
 	let key = ex11::gen_aes128_key().to_vec();
 	let mut _nonce = [0u8; 8];
@@ -73,21 +100,21 @@ pub fn break_random_access_read_write() {
 	let mut known_bytes = Vec::new();
 	let mut bytes_left = encrypted.len();
 	// for each block
-	for i in 0..utils::ceil(encrypted.len(), 16) {
+	for i in 0..utils::ceil(encrypted.len(), block_size) {
 		
-		let lower_bound = i*16;
-		let upper_bound = i*16 + utils::min(16, bytes_left);
+		let lower_bound = i*block_size;
+		let upper_bound = i*block_size + utils::min(block_size, bytes_left);
 		// println!("lower bound = {}, upper bound = {}", lower_bound, upper_bound);
 
 		// for each byte of block
 		for test_byte_index in 0..(upper_bound - lower_bound) {
-			let reference_ct = encrypter.edit(&mut encrypted.clone(), &((lower_bound + test_byte_index + 1) as u64), &vec![b'0'; 16-1-test_byte_index]);
+			let reference_ct = encrypter.edit(&mut encrypted.clone(), lower_bound + test_byte_index + 1, &vec![b'0'; block_size-1-test_byte_index]);
 
 			// for all possible values
 			for b in 0..=255 {
 				let mut test_data = vec![b];
-				test_data.append(&mut vec![b'0'; 16-1-test_byte_index]);
-				let test_ct = encrypter.edit(&mut encrypted.clone(), &((lower_bound + test_byte_index) as u64), &test_data);
+				test_data.append(&mut vec![b'0'; block_size-1-test_byte_index]);
+				let test_ct = encrypter.edit(&mut encrypted.clone(), lower_bound + test_byte_index, &test_data);
 				if test_ct == reference_ct {
 					print!("{}", b as char);
 					io::stdout().flush().expect("could not flush stdout");
@@ -98,14 +125,13 @@ pub fn break_random_access_read_write() {
 		}
 		bytes_left -= upper_bound - lower_bound
 	}
-	println!("{:?}", String::from_utf8_lossy(&known_bytes));
+	// println!("{}", String::from_utf8_lossy(&known_bytes));
 }
 
 mod tests {
 	#[test]
 	fn test_edit() {
-		use crate::ex11;
-		use crate::set3::ex18;
+		use crate::{ex11, ex18};
 		use rand::{Rng, thread_rng};
 		
 		let plaintext = "HEY THERE THIS HERE'S THE TEXT WE'RE GONNA ENCRYPT".as_bytes().to_vec();
@@ -117,7 +143,7 @@ mod tests {
 		let encrypter = super::CtrEncrypter::new(&key, &nonce);
 
 		let encrypted = ex18::encrypt_ctr(&plaintext, &key, &nonce);
-		let edited = encrypter.edit(&encrypted, &4, &"what's up".bytes().collect());
+		let edited = encrypter.edit(&encrypted, 4, &"what's up".bytes().collect());
 
 		let unencrypted = ex18::encrypt_ctr(&edited, &key, &nonce);
 		let res: Vec<u8> = "HEY what's upS HERE'S THE TEXT WE'RE GONNA ENCRYPT".bytes().collect();
