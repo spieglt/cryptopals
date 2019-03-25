62f88a770aba7a09be4ce07728328f00719094bd
Theron <tspiegl@gmail.com>
2019-03-24 20:08:46 -0500

    ex28 solved I think?

--- src/set4/ex28.rs
+++ src/set4/ex28.rs
@@ -18,31 +18,41 @@ use rand::{Rng, thread_rng};
 use sha1::{Sha1, Digest};
 
 struct Sha1KeyedMac {
-    key: Vec<u8>
+	key: Vec<u8>
 }
 
 impl Sha1KeyedMac {
-    pub fn new(key: &Vec<u8>) -> Sha1KeyedMac {
-        Sha1KeyedMac{
-            key: key.clone()
-        }
-    }
+	pub fn new(key: &Vec<u8>) -> Sha1KeyedMac {
+		Sha1KeyedMac{
+			key: key.clone()
+		}
+	}
 
-    pub fn gen(&self, message: &Vec<u8>) -> Vec<u8> {
-        let mut inp = self.key.clone();
-        inp.append(&mut message.clone());
+	pub fn gen(&self, message: &Vec<u8>) -> Vec<u8> {
+		let mut inp = self.key.clone();
+		inp.append(&mut message.clone());
 
-        let mut hasher = Sha1::new();
-        hasher.input(inp);
-        hasher.result().to_vec()
-    }
+		let mut hasher = Sha1::new();
+		hasher.input(inp);
+		hasher.result().to_vec()
+	}
+
+	pub fn authenticate(&self, mac: &Vec<u8>, message: &Vec<u8>) -> bool {
+		self.gen(message) == *mac
+	}
 }
 
-pub fn sha1_keyed_mac() { 
-    let key = (0..16).map(|_| thread_rng().gen::<u8>()).collect();
-    println!("key: {:02x?}", key);
+pub fn sha1_keyed_mac() {
+	let key = (0..16).map(|_| thread_rng().gen::<u8>()).collect();
+	let s1km = Sha1KeyedMac::new(&key);
+	let message = &b"very important data indeed".to_vec();
+	let mac = s1km.gen(message);
 
-    let s1km = Sha1KeyedMac::new(&key);
-    println!("{:02x?}", s1km.gen(&b"very important data indeed".to_vec()));
-
+	assert!(s1km.authenticate(&mac, message));
+	// Verify that you cannot tamper with the message without breaking the MAC you've produced
+	assert!(!s1km.authenticate(&mac, &b"not so important data".to_vec()));
+	// and that you can't produce a new MAC without knowing the secret key.
+	let different_s1km = Sha1KeyedMac::new(&(0..16).map(|_| thread_rng().gen::<u8>()).collect());
+	assert!(mac != different_s1km.gen(message));
+	println!("authenticate function works");
 }
